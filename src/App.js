0e2ee7c19a4b73f2da05a4cc1810330bcf5ac534
Isa SUMER <sumerisa5@gmail.com>
2021-09-29 00:01:37 +0300

    app js and other files

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import Home from "./pages/Home";
+import Result from "./pages/Result";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+function App() {
+  return (
+    <Router>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route exact path="/result">
+        <Result />
+      </Route>
+    </Router>
+  );
+}
+
+export default App;
