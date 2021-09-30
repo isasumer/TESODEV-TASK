e1b77059650e2160e48afb059e0924e40e7bf052
Isa SUMER <sumerisa5@gmail.com>
2021-09-30 21:39:11 +0300

    index.js provided with reducer

--- src/index.js
+++ src/index.js
@@ -1,22 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import {createStore} from "redux";
-import reducer from "./reducers/reducer"
-import {Provider } from "react-redux"
+import ReactDOM from "react-dom";
+import App from "./App";
+// import reportWebVitals from "./reportWebVitals";
+import { createStore } from "redux";
+import reducer from "./reducers/reducer";
+import { Provider } from "react-redux";
 
-const store= createStore(reducer)
-
+const store = createStore(reducer);
 ReactDOM.render(
-  <React.StrictMode>
     <Provider store={store}>
-    <App />
-    </Provider>
-  </React.StrictMode>,
-  document.getElementById('root')
+      <App />
+    </Provider>,
+  document.getElementById("root")
 );
-
-
-reportWebVitals();
+// reportWebVitals();
