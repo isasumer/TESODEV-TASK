8d5beaf1c1872c468eb88b0d227ed629c01ee725
Isa SUMER <sumerisa5@gmail.com>
2021-09-30 21:38:09 +0300

    app.js upgraded

--- src/App.js
+++ src/App.js
@@ -4,14 +4,9 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <Route exact path="/">
-        <Home />
-      </Route>
-      <Route exact path="/result">
-        <Result />
-      </Route>
+      <Route exact path="/">  <Home/>        </Route>
+      <Route path="/result">  <Result/>      </Route>
     </Router>
   );
 }
-
 export default App;
