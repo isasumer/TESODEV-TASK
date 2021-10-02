da2bdb1da03cc4a9bd3183fdfd003981fb45ce8e
Isa SUMER <sumerisa5@gmail.com>
2021-10-02 16:25:19 +0300

    dep

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import App from "./App";
 import { createStore } from "redux";
 import reducer from "./reducers/reducer";
 import { Provider } from "react-redux";
-import { BrowserRouter } from "react-router-dom";
 
 const store = createStore(reducer);
 ReactDOM.render(
-  <BrowserRouter basename={process.env.PUBLIC_URL}>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
