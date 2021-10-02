042c1f9ae370254603b248e3dff360348bdb7d05
Isa SUMER <sumerisa5@gmail.com>
2021-10-02 16:12:42 +0300

    deployment

--- src/App.js
+++ src/App.js
@@ -4,8 +4,8 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 function App() {
   return (
     <Router>
-      <Route exact path="/">  <Home/>        </Route>
-      <Route path="/result">  <Result/>      </Route>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/result" component={Result} />
     </Router>
   );
 }
